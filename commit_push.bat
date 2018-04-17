d89f4c980532a65d473cb0f33b2891292cee3dcc
sinkyuho <bb7305@naver.com>
2018-04-17 10:18:22 +0900

    [2017-04-17] Node.js Test

---
+++ commit_push.bat
@@ -0,0 +1,3 @@
+git add --all
+git commit -m "[2017-04-17] Node.js Test"
+git push -u origin master
---
+++ index.js
@@ -0,0 +1,3 @@
+console.log('Hello, world');
+console.log(process.version);   //버전
+console.log(process.memoryUsage());   //잡아먹는 메모리
