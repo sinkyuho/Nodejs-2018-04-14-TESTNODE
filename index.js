1ff05cd74a2b6cc1422e2cbbfe89363b7219a384
sinkyuho <bb7305@naver.com>
2018-04-17 10:58:11 +0900

    [2017-04-17] Node.js Test

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const circle = require('./circle');
 console.log('Area of a circle = ' + circle.area(3));
 console.log('Circumfrence of a circle=' +circle.circum(3));
 console.log('PI = ' +circle.pi);
+
+const square = require('./square');
+let mySquare = square(4);
+console.log('Area of a square =' + mySquare.area());
---
+++ square.js
@@ -0,0 +1,7 @@
+module.exports = function (width) {
+    return {
+        area: function () {
+            return width * width;
+        }
+    };
+};                   //정사각형 넓이!  {} 이건 객체  [] 이건 배열
