f00201c33485b019dd71834cbd689b9aad277dd4
sinkyuho <bb7305@naver.com>
2018-05-02 12:49:34 +0900

    [2017-04-17] Node.js Test

--- myServer.js
+++ myServer.js
@@ -1,19 +1,21 @@
 const http = require('http');
 const url = require('url');
-const sHost = 'localhost';
-const nPort = 8000;
 
-function start(port, hostname, route) {
-    function onRequest(req, res){
-        let Spathname = url.parse(req.url).pathname;
-        console.log('url : ' +req.url)
-        route(Spathname);
-        res.writeHead(200, {'Content-type': 'text/html'});
-        res.write('Hello, world');
-        res.end();
+exports.start = function (port, host, route, handle) {
+    function onRequest(req, res) { 
+        let sPostData = '';
+        let sPathname = url.parse(req.url).pathname;
+
+        req.setEncoding('utf-8');
+        req.addListener('data', function (dataChunk) {
+            sPostData += dataChunk;
+            console.log('Chunk = ' + dataChunk);
+        });
+        req.addListener('end', function () {
+            route(sPathname, handle, res, sPostData);
+        });
     }
-    http.createServer(onRequest).listen(port, hostname);
-    console.log('Server is renning at http://' + hostname + ':' + port);
-}
 
-exports.start = start;
+    http.createServer(onRequest).listen(port, host);
+    console.log('Server is running at ' + host + ':' + port);
+}
